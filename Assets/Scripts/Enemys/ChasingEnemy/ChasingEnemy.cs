3d06464d88095295cf1752b02022847b0b8f4ce2
meeshee126 <47327880+meeshee126@users.noreply.github.com>
2020-11-30 18:12:05 +0100

    Addings
    Added Snake script and pref (working but not dne yet)

--- Assets/Scripts/Enemys/ChasingEnemy/ChasingEnemy.cs
+++ Assets/Scripts/Enemys/ChasingEnemy/ChasingEnemy.cs
@@ -4,35 +4,15 @@ using UnityEngine;
 
 public class ChasingEnemy : EnemyManager
 {
-    [SerializeField] float frequency = 10.0f; // Speed of sine movement
-    [SerializeField] float magnitude = 0.5f; //  Size of sine movement
-    [SerializeField] bool chase;
-    Vector3 pos;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        pos = transform.position;
-        RotateToPlayer();
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if(chase)
-        RotateToPlayer();
-
-        Move();         
-    }
-
-    void Move()
-    {
-        pos += transform.right * speed * Time.deltaTime;
-        transform.position = pos + (transform.up * Mathf.Sin(frequency * Time.time) * magnitude);
-    }
-
-    void RotateToPlayer()
-    {
-        Vector3 difference = player.transform.position - this.transform.position;
-        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        this.transform.rotation = Quaternion.Euler(0, 0, rotationZ);
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Enemys/SnakeEnemy/SnakeEnemy.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeEnemy : EnemyManager
+{
+    [SerializeField] GameObject bodyPrefab;
+    [SerializeField] List<Transform> bodyParts = new List<Transform>();
+    [SerializeField] int addSize;
+    [SerializeField] float minDistance = 0.25f;
+    [Space]
+    [SerializeField] float frequency; // Speed of sine movement
+    [SerializeField] float magnitude; //  Size of sine movement
+    [SerializeField] bool chase;
+
+    float distance;
+    Transform curBodyPart;
+    Transform prevBodyPart;
+
+    Vector3 pos;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+
+        for (int i = 0; i < addSize - 1; i++)
+        {
+            AddBodyPart();
+        }
+
+        pos = transform.position;
+        RotateToPlayer();
+    }
+
+    void Update()
+    {
+        if(chase)
+        RotateToPlayer();
+
+        Move();         
+    }
+
+    void AddBodyPart()
+    {
+        Transform newPart = (Instantiate(bodyPrefab, bodyParts[bodyParts.Count - 1].position, bodyParts[bodyParts.Count - 1].rotation) as GameObject).transform;
+
+        newPart.SetParent(transform.parent);
+
+        bodyParts.Add(newPart);
+    }
+
+    void Move()
+    {
+        float curSpeed = speed;
+
+        pos += transform.right * speed * Time.deltaTime;
+        transform.position = pos + (transform.up * Mathf.Sin(frequency * Time.time) * magnitude);
+
+        for (int i = 1; i < bodyParts.Count; i++)
+        {
+            curBodyPart = bodyParts[i];
+            prevBodyPart = bodyParts[i - 1];
+
+            distance = Vector2.Distance(prevBodyPart.position, curBodyPart.position);
+
+            Vector2 newPos = prevBodyPart.position;
+
+           // newPos.y = bodyParts[0].position.y;
+
+            float time = Time.deltaTime * distance / minDistance * curSpeed;
+
+            if (time > 0.5f)
+                time = 0.5f;
+
+            curBodyPart.position = Vector3.Slerp(curBodyPart.position, newPos, time);
+            curBodyPart.rotation = Quaternion.Slerp(curBodyPart.rotation, prevBodyPart.rotation, time);
+        }
+    }
+
+    //face player
+    void RotateToPlayer()
+    {
+        Vector3 difference = player.transform.position - this.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.Euler(0, 0, rotationZ);
+    }
+}
